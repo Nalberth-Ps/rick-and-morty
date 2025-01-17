e025490dd24b63e42ac14c26e06f1cbd395a6958
Nalberth <nalberthsantosr@gmail.com>
2025-01-16 22:22:21 -0300

    feat: add terser for JavaScript minification and remove manual chunking configuration

--- vite.config.ts
+++ vite.config.ts
@@ -36,37 +36,6 @@ export default defineConfig({
 			output: {
 				entryFileNames: "assets/[name].js",
 				chunkFileNames: "assets/[name].js",
-				manualChunks(id) {
-					if (id.includes("/node_modules/")) return "dependencies"
-
-					if (id.includes("/src/context/")) {
-						const name = id
-							.split("/src/context/")[1]
-							.split(".")[0]
-							.toLowerCase()
-						return `${name}.context`
-					}
-
-					if (id.includes("/src/hooks/")) {
-						const name = id.split("/src/hooks/")[1].split(".")[0].toLowerCase()
-						return `${name}.hook`
-					}
-
-					if (id.includes("/src/pages/")) {
-						const name = id.split("/src/pages/")[1].split("/")[0].toLowerCase()
-						return `${name}.page`
-					}
-
-					if (id.includes("/src/components/")) {
-						const name = id
-							.split("/src/components/")[1]
-							.split("/")[0]
-							.toLowerCase()
-						return `${name}.component`
-					}
-
-					return null
-				},
 			},
 		},
 	},
