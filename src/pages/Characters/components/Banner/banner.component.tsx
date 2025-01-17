4de2d30a8e3e4a4382638a7a6d0182b511ebd8ab
Nalberth <nalberthsantosr@gmail.com>
2025-01-17 13:34:30 -0300

    feat: integrate react-helmet for improved SEO and preload Rick and Morty banner image

--- src/pages/Characters/components/Banner/banner.component.tsx
+++ src/pages/Characters/components/Banner/banner.component.tsx
@@ -1,16 +1,29 @@
+import { Helmet } from "react-helmet"
 import RickAndMorty from "@assets/images/rick-and-morty.svg"
 import classNames from "classnames"
 import styles from "./banner.module.css"
 
 export const Banner = () => {
 	return (
-		<div className={classNames(styles.banner)}>
-			<img
-				src={RickAndMorty}
-				alt="Rick and Morty Banner"
-				height={200}
-				width={600}
-			/>
-		</div>
+		<>
+			<Helmet>
+				<link
+					rel="preload"
+					as="image"
+					href={RickAndMorty}
+					type="image/svg+xml"
+				/>
+			</Helmet>
+			<div className={classNames(styles.banner)}>
+				<img
+					src={RickAndMorty}
+					alt="Rick and Morty Banner"
+					height={200}
+					width={600}
+					fetchPriority="high"
+					loading="eager"
+				/>
+			</div>
+		</>
 	)
 }
