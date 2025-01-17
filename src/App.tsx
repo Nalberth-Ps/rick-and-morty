d91beb32e793c4e91d14d722d58bac526bff4ea2
Nalberth <nalberthsantosr@gmail.com>
2025-01-16 23:17:34 -0300

    feat: add react-loading-skeleton for loading state representation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ApolloProvider } from "@apollo/client"
 
 import { client } from "@graphql/client"
 import { GlobalLayout } from "@layouts/GlobalLayout/global-layout.component"
+import "react-loading-skeleton/dist/skeleton.css"
 
 export const App = () => {
 	return (
